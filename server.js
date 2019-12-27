8930dcf2f7b591d0047bd29196f151bcf7155489
Kota Watanabe <kota3316@gmail.com>
2019-12-26 17:16:06 -0800

    implement crud routes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users:[
@@ -22,6 +25,14 @@ const database = {
           entries:0,
           joined:new Date()
         }
+    ],
+    login: [
+        {
+            id:'987',
+            hash:'',
+            email:'john@gmail.com'
+        }
+
     ]
 }
 
@@ -30,6 +41,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req,res) => {
+    bcrypt.compare("apples", "$2a$10$iBk5HNfsiWz/Em9sHu3IA./s3.p6rvYcFxgumWbozokTEj/vkHV7O", function(err, res) {
+        console.log('first guess', res)
+    });
+    bcrypt.compare("veggies", "$2a$10$iBk5HNfsiWz/Em9sHu3IA./s3.p6rvYcFxgumWbozokTEj/vkHV7O", function(err, res) {
+        console.log('second guess', res)
+    });
     if(req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password){
     res.json('success')
@@ -40,11 +57,13 @@ app.post('/signin', (req,res) => {
 
 app.post('/register', (req,res)=> {
     const { email, name, password } = req.body
+    // bcrypt.hash(password, null, null, function(err, hash) {
+    //    console.log(hash)
+    // });
     database.users.push({
         id:'125',
         name,
         email,
-        password,
         entries:0,
         joined:new Date()
     })
@@ -65,7 +84,7 @@ app.get('/profile/:id', (req,res) => {
     }
 })
 
-app.post('/image', (req,res) => {
+app.put('/image', (req,res) => {
     const { id } = req.body;
     let found = false;
     database.users.forEach(user => {
@@ -79,6 +98,7 @@ app.post('/image', (req,res) => {
         res.status(400).json('not found');
     }
 })
+
 
 app.listen(3000,() => {
     console.log('app is running on port 3000');
